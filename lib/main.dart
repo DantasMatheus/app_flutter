1d97984c1a756a203e818e2f0bf241d40f90ebcd
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-02-20 15:43:42 -0300

    Adição de dificuldade

--- lib/main.dart
+++ lib/main.dart
@@ -28,35 +28,41 @@ class MyApp extends StatelessWidget {
             Task(
               'Aprender Flutter',
               'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+              3,
             ),
             Task(
               'Aprender Dart',
               'https://m.media-amazon.com/images/I/51z07YgHRBL.jpg',
+              3,
             ),
             Task(
               'Desenvolver App',
               'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxna5cYeZyX22C7YSdVpICKlblUQ6xgewGBA&s',
+              4,
             ),
             Task(
               'Alimentar o cachorro',
               'https://static.vecteezy.com/system/resources/previews/005/293/990/non_2x/pet-food-logo-with-dog-icon-suitable-for-pet-shop-and-vet-free-vector.jpg',
+              1,
             ),
             Task(
               'Aprender Estrutura de Dados',
               'https://media.licdn.com/dms/image/D5612AQG1pE_H-m9TgQ/article-cover_image-shrink_720_1280/0/1677414547742?e=2147483647&v=beta&t=TkQybfOZuzU9MyNSYbpqUg0eyEJoALfwEyTpwmnJZgg',
+              4,
             ),
             Task(
               'Meditar',
               'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+              4,
             ),
             Task(
               'Fazer Café',
               'https://cdn.pixabay.com/photo/2022/03/09/05/05/coffee-7057030_1280.png',
+              2,
             ),
           ],
         ),
-
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        //floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
   }
@@ -65,7 +71,9 @@ class MyApp extends StatelessWidget {
 class Task extends StatefulWidget {
   final String nome;
   final String foto;
-  const Task(this.nome, this.foto, {super.key});
+  final int dificuldade;
+
+  const Task(this.nome, this.foto, this.dificuldade, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -95,15 +103,66 @@ class _TaskState extends State<Task> {
                       child: Image.network(widget.foto, fit: BoxFit.cover),
                     ),
 
-                    SizedBox(
-                      width: 200,
-                      child: Text(
-                        widget.nome,
-                        style: TextStyle(
-                          fontSize: 24,
-                          overflow: TextOverflow.ellipsis,
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
                         ),
-                      ),
+
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color:
+                                  widget.dificuldade >= 1
+                                      ? Colors.blue
+                                      : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color:
+                                  widget.dificuldade >= 2
+                                      ? Colors.blue
+                                      : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color:
+                                  widget.dificuldade >= 3
+                                      ? Colors.blue
+                                      : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color:
+                                  widget.dificuldade >= 4
+                                      ? Colors.blue
+                                      : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color:
+                                  widget.dificuldade >= 5
+                                      ? Colors.blue
+                                      : Colors.blue[100],
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                     SizedBox(
                       height: 52,
@@ -145,7 +204,10 @@ class _TaskState extends State<Task> {
                       child: LinearProgressIndicator(
                         backgroundColor: Colors.grey,
                         color: Colors.white,
-                        value: nivel / 10,
+                        value:
+                            widget.dificuldade > 0
+                                ? (nivel / widget.dificuldade) / 10
+                                : 1,
                       ),
                     ),
                   ),
