a37e9dfa856457d9e06ef71bd68d8f40e397d420
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-02-25 12:05:51 -0300

    troca do Image.network para Image.asset, podendo pegar uma imagem direto do projeto ao invés de pegar da internet

--- lib/screens/inicial_screen.dart
+++ lib/screens/inicial_screen.dart
@@ -23,41 +23,18 @@ class _InicialScreenState extends State<InicialScreen> {
         duration: Duration(milliseconds: 800),
         child: ListView(
           children: [
-            Task(
-              'Aprender Flutter',
-              'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-              3,
-            ),
-            Task(
-              'Aprender Dart',
-              'https://m.media-amazon.com/images/I/51z07YgHRBL.jpg',
-              3,
-            ),
-            Task(
-              'Desenvolver App',
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxna5cYeZyX22C7YSdVpICKlblUQ6xgewGBA&s',
-              4,
-            ),
-            Task(
-              'Alimentar o cachorro',
-              'https://static.vecteezy.com/system/resources/previews/005/293/990/non_2x/pet-food-logo-with-dog-icon-suitable-for-pet-shop-and-vet-free-vector.jpg',
-              1,
-            ),
+            Task('Aprender Flutter', 'assets/images/flutter.png', 3),
+            Task('Aprender Dart', 'assets/images/dart.jpg', 3),
+            Task('Desenvolver App', 'assets/images/android.jpg', 4),
+            Task('Alimentar o cachorro', 'assets/images/cachorro.jpg', 1),
             Task(
               'Aprender Estrutura de Dados',
-              'https://media.licdn.com/dms/image/D5612AQG1pE_H-m9TgQ/article-cover_image-shrink_720_1280/0/1677414547742?e=2147483647&v=beta&t=TkQybfOZuzU9MyNSYbpqUg0eyEJoALfwEyTpwmnJZgg',
+              'assets/images/data_structure.jpg',
               4,
             ),
-            Task(
-              'Meditar',
-              'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-              4,
-            ),
-            Task(
-              'Fazer Café',
-              'https://cdn.pixabay.com/photo/2022/03/09/05/05/coffee-7057030_1280.png',
-              2,
-            ),
+            Task('Meditar', 'assets/images/meditar.jpeg', 5),
+            Task('Fazer Café', 'assets/images/coffee.jpg', 2),
+            SizedBox(height: 80),
           ],
         ),
       ),
