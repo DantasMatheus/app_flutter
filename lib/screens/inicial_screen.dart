aa57a3447747ec4bccf26cbd5404ab1e9bf0e507
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-03-04 14:42:33 -0300

    Desafio Final Concluído!

--- lib/screens/inicial_screen.dart
+++ lib/screens/inicial_screen.dart
@@ -10,13 +10,67 @@ class InicialScreen extends StatefulWidget {
 }
 
 class _InicialScreenState extends State<InicialScreen> {
+  double globalLevel = 0;
+  double progressBarValue = 0;
+
+  void refreshLevelPage() {
+    setState(() {
+      globalLevel = 0; //para resetar os valores armazenados em cache
+      progressBarValue = 0; //para resetar os valores armazenados em cache
+      for (var task in TaskInherited.of(context)?.taskList ?? []) {
+        final taskLevel = (task.nivel * task.dificuldade) / 10;
+        globalLevel += taskLevel;
+      }
+      progressBarValue = globalLevel / 100;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.add_task, color: Colors.white),
         backgroundColor: Colors.blue,
-        title: Text('Tarefas', style: TextStyle(color: Colors.white)),
+        title: Text(
+          ' Tarefas',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(15),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 180,
+                  child: LinearProgressIndicator(
+                    backgroundColor: Colors.grey,
+                    color: Colors.white,
+                    value: progressBarValue,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    'Level Global: ${globalLevel.toStringAsFixed(2)}',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 50.0),
+            child: IconButton(
+              icon: Icon(Icons.loop),
+              onPressed: () {
+                refreshLevelPage();
+              },
+            ),
+          ),
+        ],
       ),
       body: ListView(
         padding: EdgeInsets.only(top: 8, bottom: 70),
